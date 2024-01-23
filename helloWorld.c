f59821c7691b9949762e576e46f50ce86b2298ae
bhavesh0134 <bhaveshthombre111@gmail.com>
2024-01-23 20:27:28 +0530

    Add code to find minimum of two numbers

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main() {
     printf("Hello, World!");
+    int a = 5, b = 10;
+    int min = (a < b) ? a : b;
+    printf("The minimum of two numbers is: %d\n",min);
     return 0;
 }
